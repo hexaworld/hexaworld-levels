3efbe6b45a86b218035f862cd32b67ffddb21df1
sethvincent <sethvincent@gmail.com>
2016-01-18 20:47:46 -0800

    use bulk-require/bulkify to make it easy to require all levels with browserify

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-var fs = require('fs')
-var path = require('path')
-
-var _ = require('lodash')
-
-var levelsPath = path.join(__dirname, 'levels')
-var levelNames = fs.readdirSync(levelsPath)
-var getName = function (level) {
-  return level.slice(0, -5)
-}
-var getJson = function (level) {
-  return JSON.parse(fs.readFileSync(path.join(levelsPath, level)))
-}
-module.exports = _.zipObject(levelNames.map(function (level) { return getName(level) }),
-                             levelNames.map(function (level) { return getJson(level) }))
+var bulk = require('bulk-require')
+module.exports = bulk(__dirname, 'levels/*.json').levels
